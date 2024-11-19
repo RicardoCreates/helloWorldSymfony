e715452215d4c2a5ce74c304fc93523afdab5c8a
Ricardo Lemser <ricardo.lemser@gmail.com>
2024-11-19 15:52:16 +0100

    Recreate to Service Website - created AboutController and index - modified Greeting to Service and also index - modified HomePage and index

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -1,16 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class GreetingController extends AbstractController
+class AboutController extends AbstractController
 {
-    #[Route('/Services', name: 'Services')]
-    public function greet(): Response
+    #[Route('/about')]
+    public function index(): Response
     {
-        return $this->render('Services/index.html.twig');
+        return $this->render('about/index.html.twig');
     }
 }
